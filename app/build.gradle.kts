7823ce33d732c3ed7726f23f1bc774f8ba55d315
jadjer <mail@jadjer.by>
2026-09-01 18:16:13 +0300

    Рефакторинг Добавлена история измерения по транспортным средствам Добавлены локализации Обновлена целевая версия android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "by.jadjer.valveclearance"
         minSdk = 29
         targetSdk = 37
-        versionCode = 12
+        versionCode = 13
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
